a5d8935edd281542b7df09ba6051b7be997bd04e
Lars Huring <lars.huring@gmail.com>
2017-09-21 16:06:15 +0200

    Testing debug

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log("Kalle")
+console.log("Kalle");
+console.log("Foo");
+
+var arr = [1,3,5,8];
+
+for (var index = 0; index < arr.length; index++) {
+    var element = arr[index];
+    console.log(element);
+}
